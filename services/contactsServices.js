3fda08270f1d346ccb624afdbc0332221e14c6bc
@7Karina <karinafrank750@gmail.com>
2024-02-16 18:06:05 +0200

    and

---
+++ services/contactsServices.js
@@ -0,0 +1,89 @@
+const fs = require('node:fs/promises');
+const path = require('node:path');
+const crypto = require('crypto');
+
+const contactsPath = path.join(__dirname, 'db', 'contacts.json');
+console.log(contactsPath);
+
+async function readContacts() {
+  try {
+    const data = await fs.readFile(contactsPath, { encoding: 'utf-8' });
+    return JSON.parse(data);
+  } catch (error) {
+    console.error('Error reading contacts:', error);
+    throw error;
+  }
+}
+
+async function writeContacts(contacts) {
+  try {
+    const data = JSON.stringify(contacts, null, 2);
+    await fs.writeFile(contactsPath, data);
+  } catch (error) {
+    console.error('Error writing contacts:', error);
+    throw error;
+  }
+}
+
+async function listContacts() {
+  try {
+    const contacts = await readContacts();
+    return contacts;
+  } catch (error) {
+    console.error('Error listing contacts:', error);
+    throw error;
+  }
+}
+
+async function getContactById(contactId) {
+  try {
+    const contacts = await readContacts();
+    const contact = contacts.find(contact => contact.id === contactId);
+    return contact || null;
+  } catch (error) {
+    console.error('Error getting contact by ID:', error);
+    throw error;
+  }
+}
+
+async function removeContact(contactId) {
+  try {
+    const contacts = await readContacts();
+    const index = contacts.findIndex(contact => contact.id === contactId);
+    if (index !== -1) {
+      const removedContact = contacts.splice(index, 1);
+      await writeContacts(contacts);
+      return removedContact[0];
+    } else {
+      return null;
+    }
+  } catch (error) {
+    console.error('Error removing contact:', error);
+    throw error;
+  }
+}
+
+async function addContact(name, email, phone) {
+  try {
+    const contacts = await listContacts();
+    const newContact = {
+      id: crypto.randomUUID(),
+      name,
+      email,
+      phone,
+    };
+    contacts.push(newContact);
+    await writeContacts(contacts);
+    return newContact;
+  } catch (error) {
+    console.error('Error adding contact:', error);
+    throw error;
+  }
+}
+
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+};
