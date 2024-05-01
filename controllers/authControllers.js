bf9acc6e71af25f34a35a30213d72402520098a4
@7Karina <karinafrank750@gmail.com>
2024-05-01 18:45:07 +0300

    avatars

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -2,6 +2,10 @@ import { User } from '../DBModels/userModel.js';
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 import 'dotenv/config';
+import fs from 'fs/promises';
+import gravatar from 'gravatar';
+import path from 'path';
+import jimp from 'jimp';
 import HttpError from '../helpers/HttpError.js';
 const JWT_SECRET = process.env.JWT_SECRET;
 
@@ -14,7 +18,13 @@ export const register = async (req, res, next) => {
     }
 
     const hashPassword = await bcrypt.hash(password, 10);
-    const newUser = await User.create({ ...req.body, password: hashPassword });
+    const avatarURL = gravatar.url(email);
+
+    const newUser = await User.create({
+      ...req.body,
+      password: hashPassword,
+      avatarURL,
+    });
 
     res.status(201).json({
       user: {
@@ -81,4 +91,31 @@ export const currentUser = async (req, res, next) => {
   } catch (error) {
     next(error);
   }
+};
+
+export const upDateAvatar = async (req, res) => {
+  const userId = req.user._id;
+
+  if (!req.file) {
+    res.status(400).json({ message: 'the request must contain a file' });
+    return;
+  }
+
+  const tempDir = path.join(req.file.path);
+  const extname = path.extname(req.file.originalname);
+  const basename = path.basename(req.file.originalname, extname);
+  const fileName = `${basename}${userId}${extname}`;
+
+  const publicDir = path.join(process.cwd(), `public/avatar/${fileName}`);
+
+  await fs.rename(tempDir, publicDir);
+  const avatarURL = patch.join('avatars', fileName);
+
+  const image = await jimp.read(publicDir);
+  await image.resize(250, 250);
+  await image.writeAsync(publicDir);
+
+  await User.findByIdAndUpdate(userId, { avatarURL });
+
+  res.status(200).json({ avatarURL });
 };
--- routes/authRouter.js
+++ routes/authRouter.js
@@ -4,10 +4,12 @@ import {
   login,
   logout,
   currentUser,
+  upDateAvatar,
 } from '../controllers/authControllers.js';
 import { validateBody } from '../helpers/validateBody.js';
 import { schemas } from '../schemas/usersSchemas.js';
 import { validateToken } from '../helpers/validateToken.js';
+import upload from '../middleWares/avatarUpLoad.js';
 
 const authRouter = express.Router();
 
@@ -18,5 +20,12 @@ authRouter.post('/login', validateBody(schemas.registerSchema), login);
 authRouter.post('/logout', validateToken, logout);
 
 authRouter.get('/current', validateToken, currentUser);
+
+authRouter.patch(
+  '/avatars',
+  validateToken,
+  upload.single('avatar'),
+  upDateAvatar
+);
 
 export default authRouter;
