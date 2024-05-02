dda2823d3aea12c0d5b3162fdfb4b61bd25a2092
@7Karina <karinafrank750@gmail.com>
2024-05-02 13:09:33 +0300

    url gravatar

--- DBModels/userModel.js
+++ DBModels/userModel.js
@@ -25,4 +25,15 @@ const userSchema = new Schema(
   { versionKey: false }
 );
 
+userSchema.pre('save', async function (next) {
+  if (this.isNew) {
+    const emailHash = crypto.createHash('md5').update(this.email).digest('hex');
+    console.log(emailHash);
+
+    this.avatar = `http://gravatar.com/avatar/${emailHash}.jpg?d=robohash`;
+  }
+
+  next();
+});
+
 export const User = mongoose.model('user', userSchema);
